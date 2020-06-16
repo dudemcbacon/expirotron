24c855a29fca1c77ae5a500c7739cf7cb2ed1235
Brandon Burnett <gentoolicious@gmail.com>
2020-06-16 09:22:53 -0700

    python black

--- expirotron.py
+++ expirotron.py
@@ -7,78 +7,107 @@ import ssl
 import os
 import sys
 
+
 def reformat(torrents):
     parsed = []
     for i in torrents:
-        parsed.append({ i: torrents[i] })
+        parsed.append({i: torrents[i]})
     return parsed
+
 
 def filter_accepted_age(torrent):
     accepted_age = datetime.now() - timedelta(days=90)
     torrent_id = list(torrent.keys())[0]
-    time_added = datetime.fromtimestamp(torrent[torrent_id][b'time_added'])
-    name = torrent[torrent_id][b'name']
+    time_added = datetime.fromtimestamp(torrent[torrent_id][b"time_added"])
+    name = torrent[torrent_id][b"name"]
     if accepted_age > time_added:
         return True
     else:
         print(f"Filtering {name} because its age is not below the threshold")
         return False
 
+
 def filter_accepted_labels(torrent):
-    accepted = [b'tv-sonarr', b'radarr']
+    accepted = [b"tv-sonarr", b"radarr"]
     torrent_id = list(torrent.keys())[0]
-    label = torrent[torrent_id][b'label']
-    name = torrent[torrent_id][b'name']
+    label = torrent[torrent_id][b"label"]
+    name = torrent[torrent_id][b"name"]
     if label in accepted:
         return True
     else:
         print(f"Filtering {name} because its label is not accepted")
         return False
 
+
 def filter_accepted_tracker_status(torrent):
-    accepted = [b'Announce OK', b'']
+    accepted = [b"Announce OK", b""]
     torrent_id = list(torrent.keys())[0]
-    tracker_status = torrent[torrent_id][b'tracker_status']
-    name = torrent[torrent_id][b'name']
+    tracker_status = torrent[torrent_id][b"tracker_status"]
+    name = torrent[torrent_id][b"name"]
     if tracker_status not in accepted:
         return True
     else:
         print(f"Filtering {name} because its tracker status is accepted")
         return False
 
+
 def filter_ratio(torrent):
     torrent_id = list(torrent.keys())[0]
-    ratio = torrent[torrent_id][b'ratio']
-    progress = torrent[torrent_id][b'progress']
+    ratio = torrent[torrent_id][b"ratio"]
+    progress = torrent[torrent_id][b"progress"]
     if progress == 100 and ratio >= 1:
         return True
     else:
         return False
-        print(f'Filtering {name} because its ratio is not >= 1')
+        print(f"Filtering {name} because its ratio is not >= 1")
+
 
 def torrent_ids(torrent):
     return list(torrent.keys())[0]
 
-env_vars = [ 'DELUGE_IP', 'DELUGE_PASSWORD', 'DELUGE_PORT', 'DELUGE_USER',
-        'SMTP_HOST', 'SMTP_PASSWORD', 'SMTP_PORT', 'SMTP_USER' ]
+
+env_vars = [
+    "DELUGE_IP",
+    "DELUGE_PASSWORD",
+    "DELUGE_PORT",
+    "DELUGE_USER",
+    "SMTP_HOST",
+    "SMTP_PASSWORD",
+    "SMTP_PORT",
+    "SMTP_USER",
+]
 
 for env_var in env_vars:
     if os.environ.get(env_var) == None:
-        print(f'{env_var} must be defined in the environment')
+        print(f"{env_var} must be defined in the environment")
         sys.exit(1)
 
 
-keys = ['time_added', 'time_since_download', 'time_since_transfer',
-        'time_since_upload', 'total_peers', 'total_seeds', 'is_finished',
-        'is_seed', 'ratio', 'name', 'tracker_status', 'label', 'progress']
+keys = [
+    "time_added",
+    "time_since_download",
+    "time_since_transfer",
+    "time_since_upload",
+    "total_peers",
+    "total_seeds",
+    "is_finished",
+    "is_seed",
+    "ratio",
+    "name",
+    "tracker_status",
+    "label",
+    "progress",
+]
 
-client = DelugeRPCClient(os.environ.get('DELUGE_IP'),
-                        int(os.environ.get('DELUGE_PORT')),
-                        os.environ.get('DELUGE_USER'),
-                        os.environ.get('DELUGE_PASSWORD'))
+client = DelugeRPCClient(
+    os.environ.get("DELUGE_IP"),
+    int(os.environ.get("DELUGE_PORT")),
+    os.environ.get("DELUGE_USER"),
+    os.environ.get("DELUGE_PASSWORD"),
+)
 client.connect()
 
-torrents = client.call('core.get_torrents_status', {}, keys)
+torrents = client.call("core.get_torrents_status", {}, keys)
 
 reformated_torrents = reformat(torrents)
 
@@ -95,42 +124,52 @@ ratio_ids = list(map(torrent_ids, ratio))
 
 remove = []
 for id in tracker_ids:
-    torrents[id].setdefault('reason', []).append('tracker')
+    torrents[id].setdefault("reason", []).append("tracker")
     remove.append(id)
 
 for id in age_ids:
-    torrents[id].setdefault('reason', []).append('age')
+    torrents[id].setdefault("reason", []).append("age")
     remove.append(id)
 
 for id in ratio_ids:
-    torrents[id].setdefault('reason', []).append('ratio')
+    torrents[id].setdefault("reason", []).append("ratio")
     remove.append(id)
 
 names = []
 for id in remove:
-    name = torrents[id][b'name']
-    progress = round(torrents[id][b'progress'])
-    ratio = round(torrents[id][b'ratio'], 2)
-    reason = ' '.join(torrents[id]['reason'])
+    name = torrents[id][b"name"]
+    progress = round(torrents[id][b"progress"])
+    ratio = round(torrents[id][b"ratio"], 2)
+    reason = " ".join(torrents[id]["reason"])
     # names.append(f'(progress: {progress}% ratio: {ratio}) {name.decode("utf-8")}')
-    names.append({'Name': name.decode("utf-8"), 'Reason': reason, 'Progress': progress, 'Ratio': ratio})
+    names.append(
+        {
+            "Name": name.decode("utf-8"),
+            "Reason": reason,
+            "Progress": progress,
+            "Ratio": ratio,
+        }
+    )
     print(f"Removing {name}...")
-    #torrents = client.call('core.remove_torrent', id, True)
+    # torrents = client.call('core.remove_torrent', id, True)
 
-names.sort(key=lambda x: x['Name'])
+names.sort(key=lambda x: x["Name"])
 
 msg = EmailMessage()
-msg['Subject'] = 'Expirotron Report'
-msg['From'] = 'brandon@milosh.dev'
-msg['To'] = 'brandon@milosh.dev'
-msg.set_content('The following torrents have been expired due to age or torrent tracker error:\n\n' + tabulate(names, headers="keys"))
+msg["Subject"] = "Expirotron Report"
+msg["From"] = "brandon@milosh.dev"
+msg["To"] = "brandon@milosh.dev"
+msg.set_content(
+    "The following torrents have been expired due to age or torrent tracker error:\n\n"
+    + tabulate(names, headers="keys")
+)
 
 context = ssl.create_default_context()
 
-with smtplib.SMTP(os.environ.get('SMTP_HOST'), os.environ.get('SMTP_PORT')) as server:
+with smtplib.SMTP(os.environ.get("SMTP_HOST"), os.environ.get("SMTP_PORT")) as server:
     server.ehlo()
     server.starttls()
-    server.login(os.environ.get('SMTP_USER'), os.environ.get('SMTP_PASSWORD'))
+    server.login(os.environ.get("SMTP_USER"), os.environ.get("SMTP_PASSWORD"))
     server.ehlo()
     server.send_message(msg)
     server.quit()
